6582f04ce152ea11ce4e606f913a60d13bcaa95a
rajesh kumar <kumrajbiz@gmail.com>
2023-05-03 16:31:28 +0530

    to check

--- WeatherMapAPIv2-mocroservice/src/main/java/com/java/net/WeatherMapApIv2MocroserviceApplication.java
+++ WeatherMapAPIv2-mocroservice/src/main/java/com/java/net/WeatherMapApIv2MocroserviceApplication.java
@@ -10,10 +10,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableSwagger2
+
 public class WeatherMapApIv2MocroserviceApplication {
 
 	public static void main(String[] args) {
